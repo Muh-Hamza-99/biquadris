717e4d10f73e81113c65ce4d561440d04ade1a43
tinazzzz <tina.zhang005@gmail.com>
2024-11-22 13:36:00 -0500

    Update game.h

--- game.h
+++ game.h
@@ -1,14 +1,38 @@
 #ifndef _GAME_H_
 #define _GAME_H_
 
-#include "subject.h"
-#include "board.h"
 #include <memory>
+#include <string>
+#include "board.h"
+#include "score.h"
+#include "commandinterpreter.h"
+#include "observer.h"
+#include "textobserver.h"
+#include "graphicsobserver.h"
+
 using namespace std;
 
 class Game : public Subject {
-    shared_ptr<Board> board1;
-    shared_ptr<Board> board2;
+private:
+    // private fields
+    unique_ptr<Board> board1;
+    unique_ptr<Board> board2;
+    unique_ptr<Score> score;
+    unique_ptr<CommandInterpreter> commandInterpreter;
+    unique_ptr<TextObserver> textObserver;
+    unique_ptr<GraphicsObserver> graphicsObserver;
+    bool player1Turn;
+
+public:
+    // constructor and destructor
+    Game();
+    ~Game();
+
+    // public methods
+    void startGame();
+    void restartGame();
+    Bool checkPlayer1Turn();
+    void notifyObservers();
 };
 
 #endif
