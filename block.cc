51c0b97120f0c49a7f04ed4edd5824eeb301eb4d
Muhammad Hamza <codingtimelapseyt@gmail.com>
2024-11-23 13:12:36 -0500

    basic block structure and methods

--- block.cc
+++ block.cc
@@ -2,36 +2,113 @@
 
 // BLOCK
 
+Block::Block(bool heavy, int rotation): heavy{heavy}, rotation{rotation} {}
+
 Block::~Block() {}
+
+void Block::left() {
+    // Check if the block goes out of bounds
+    for (auto coord : coords) {
+        if (coord.first - 1) return;
+    }
+    for (auto &coord : coords) { coord.first -= 1; }
+    if (heavy) down();
+}
+
+void Block::right() {
+    // Check if the block goes out of bounds
+    for (auto coord : coords) {
+        if (coord.first + 1) return;
+    }
+    for (auto &coord : coords) { coord.first += 1; }
+    if (heavy) down();
+}
+
+void Block::down() {
+    for (auto &coord : coords) { coord.second += 1; }
+}
 
 // IBLOCK
 
 char IBlock::getType() const { return 'I'; }
 
+void IBlock::init() {
+    coords.emplace_back(0, 1);
+    coords.emplace_back(1, 1);
+    coords.emplace_back(2, 1);
+    coords.emplace_back(3, 1);
+}
+
 // JBLOCK
 
 char JBlock::getType() const { return 'J'; }
+
+void JBlock::init() {
+    coords.emplace_back(0, 0);
+    coords.emplace_back(0, 1);
+    coords.emplace_back(1, 1);
+    coords.emplace_back(2, 1);
+}
 
 // LBLOCK
 
 char LBlock::getType() const { return 'L'; }
 
+void LBlock::init() {
+    coords.emplace_back(2, 0);
+    coords.emplace_back(0, 1);
+    coords.emplace_back(1, 1);
+    coords.emplace_back(2, 1);
+}
+
 // OBLOCK
 
 char OBlock::getType() const { return 'O'; }
+
+void OBlock::init() {
+    coords.emplace_back(0, 1);
+    coords.emplace_back(0, 2);
+    coords.emplace_back(1, 1);
+    coords.emplace_back(1, 2);
+}
 
 // SBLOCK
 
 char SBlock::getType() const { return 'S'; }
 
+void SBlock::init() {
+    coords.emplace_back(0, 1);
+    coords.emplace_back(1, 1);
+    coords.emplace_back(1, 0);
+    coords.emplace_back(2, 0);
+}
+
 // ZBLOCK
 
 char ZBlock::getType() const { return 'Z'; }
+
+void ZBlock::init() {
+    coords.emplace_back(0, 0);
+    coords.emplace_back(1, 0);
+    coords.emplace_back(1, 1);
+    coords.emplace_back(2, 1);
+}
 
 // TBLOCK
 
 char TBlock::getType() const { return 'T'; }
 
+void TBlock::init() {
+    coords.emplace_back(1, 0);
+    coords.emplace_back(0, 1);
+    coords.emplace_back(1, 1);
+    coords.emplace_back(2, 1);
+}
+
 // ASTERIKBLOCK
 
 char AsterikBlock::getType() const { return '*'; }
+
+void AsterikBlock::init() {
+    coords.emplace_back(5, 2);
+}
--- block.h
+++ block.h
@@ -1,50 +1,71 @@
 #ifndef _BLOCK_H_
 #define _BLOCK_H_
 
+#include <vector>
+#include <memory>
+using namespace std;
+
 class Block {
+    protected:
+        bool heavy;
+        vector<pair<int, int>> coords; // Pairs of (x,y) coordinates
+        int rotation;
     public:
-        virtual char getType() const = 0;
+        Block(bool heavy = false, int rotation = 0);
+        void left();
+        void right();
+        void down();
         virtual ~Block();
+        virtual char getType() const = 0;
+        virtual void init() = 0; // To initialize starting (x,y) coordinates 
 };
 
 class IBlock : public Block {
     public:
         char getType() const override;
+        void init() override;
 };
 
 class JBlock : public Block {
     public:
         char getType() const override;
+        void init() override;
 };
 
 class LBlock : public Block {
     public:
         char getType() const override;
+        void init() override;
 };
 
 class OBlock : public Block {
     public:
         char getType() const override;
+        void init() override;
 };
 
 class SBlock : public Block {
     public:
         char getType() const override;
+        void init() override;
 };
 
 class ZBlock : public Block {
     public:
         char getType() const override;
+        void init() override;
 };
 
 class TBlock : public Block {
     public:
         char getType() const override;
+        void init() override;
 };
 
 class AsterikBlock : public Block {
     public:
         char getType() const override;
+        void init() override;
 };
 
 #endif
