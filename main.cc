4e4454a2a95bd3067cd042cf155a93e9745cf162
Muhammad Hamza <codingtimelapseyt@gmail.com>
2024-11-23 12:03:35 -0500

    basic command handler

--- main.cc
+++ main.cc
@@ -6,7 +6,7 @@
 #include "text.h"
 using namespace std;
 
-int main() {
+int main(int argc, char* argv[]) {
     cout << "-------------------------------------" << endl;
     cout << "              BIQUADRIS              " << endl;
     cout << "-------------------------------------" << endl;
@@ -23,5 +23,42 @@ int main() {
     shared_ptr<Text> textObserver { new Text(game) };
     game->attach(textObserver);
 
-    game->notifyObservers();
+    game->render();
+
+    while (true) {
+        string command;
+        cin >> command;
+
+        if (command == "left") {
+            game->render();
+        } else if (command == "right") {
+            game->render();
+        } else if (command == "down") {
+            game->render();
+        } else if (command == "clockwise") {
+            game->render();
+        } else if (command == "counterclockwise") {
+            game->render();
+        } else if (command == "drop") {
+            game->render();            
+        } else if (command == "levelup") {
+            cout << "Game level increased to X" << endl;
+        } else if (command == "leveldown") {
+            cout << "Game level increased to X" << endl;
+        } else if (command == "random") {
+
+        } else if (command == "norandom") {
+
+        } else if (command == "random") {
+
+        } else if (command == "sequence") {
+
+        } else if (command == "restart") {
+            
+        } else if (command == "quit") {
+            break;
+        } else {
+            cout << "Invalid command. Please input a valid command.";
+        }
+    }
 }
