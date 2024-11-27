497677c63dae51f18db54dabe5a05b4194ac86f9
Tina <t77zhang@uwaterloo.ca>
2024-11-27 18:39:19 -0500

    f

--- graphic.cc
+++ graphic.cc
@@ -40,15 +40,8 @@ void Graphic::drawBlock(const std::vector<std::vector<char>>& block, int xOffset
     for (size_t i = 0; i < block.size(); ++i) {
         for (size_t j = 0; j < block[i].size(); ++j) {
             char cellType = block[i][j];
-            if (cellType != ' ') {
-                int color = win.cellTypeToColor(cellType);
-                win.fillRectangle(
-                    xOffset + j * cellSize,  
-                    yOffset + i * cellSize, 
-                    cellSize, cellSize,      
-                    color                   
-                );
-            }
+            int color = win.cellTypeToColor(cellType);
+            win.fillRectangle(xOffset + j * cellSize, yOffset + i * cellSize, cellSize, cellSize);
         }
     }
 }
